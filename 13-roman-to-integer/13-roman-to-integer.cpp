1e7c073a37eb60e2735f25c4c5b516a96968eca8
Meow Lu <46015470+jasmine0122@users.noreply.github.com>
2022-03-25 16:00:57 +0800

    Time: 8 ms (83.34%), Space: 5.9 MB (88.48%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,31 +1,56 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char,int> m;
-        m['I']=1;
-        m['V']=5;
-        m['X']=10;
-        m['L']=50;
-        m['C']=100;
-        m['D']=500;
-        m['M']=1000;
+        int sum = 0;
         
-        int n = s.length();
-        int ans=0;
-        for(int i=0; i<n; )
-        {
-            // 'IX'= 10-1=9
-            // 'XL'= 50-10=40
-            // 'IV'= 5-1=4
-            if(i+1 < n && m[s[i]] < m[s[i+1]]){
-                ans = ans + m[s[i+1]] - m[s[i]];
-                i += 2;
-            }  
-            else{
-                ans = ans + m[s[i]];
-                i++;
+        for(int i=0; i<s.length() ;i++){
+            if(s[i] == 'A')
+                continue;
+            if(s[i] == 'I'){
+                if(s[i+1] == 'V'){
+                    sum += 4;
+                    s[i+1] = 'A';
+                }
+                else if(s[i+1] == 'X'){
+                    sum += 9;
+                    s[i+1] = 'A';
+                }
+                else
+                    sum += 1;  
             }
+            else if(s[i] == 'X'){
+                if(s[i+1] == 'L'){
+                    sum += 40;
+                    s[i+1] = 'A';
+                }
+                else if(s[i+1] == 'C'){
+                    sum += 90;
+                    s[i+1] = 'A';
+                }
+                else
+                    sum += 10;                     
+            }
+            else if(s[i] == 'C'){
+                if(s[i+1] == 'D'){
+                    sum += 400;
+                    s[i+1] = 'A';
+                }
+                else if(s[i+1] == 'M'){
+                    sum += 900;
+                    s[i+1] = 'A';
+                }
+                else
+                    sum += 100;                     
+            }
+            else if(s[i] == 'V')
+                sum += 5;
+            else if(s[i] == 'L')
+                sum += 50;           
+            else if(s[i] == 'D')
+                sum += 500;
+            else if(s[i] == 'M')
+                sum += 1000;
         }
-        return ans;
+        return sum;
     }
 };
