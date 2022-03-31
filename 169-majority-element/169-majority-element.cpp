1272ee24d7e0ffe0836bd45958f17828e622e58e
Meow Lu <46015470+jasmine0122@users.noreply.github.com>
2022-03-31 20:43:02 +0800

    Time: 23 ms (66.25%), Space: 19.5 MB (98.04%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -3,7 +3,6 @@ public:
     int majorityElement(vector<int>& nums) {
         map<int, int> m;
         int L = nums.size()/2;
-        //cout<<L<<endl;
         
         for(int i=0; i<nums.size(); i++){
             m[nums[i]]++;
