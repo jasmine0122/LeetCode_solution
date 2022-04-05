59d8ef4a3e4961c08bfaf52de40e39b497fcc4b4
Meow Lu <46015470+jasmine0122@users.noreply.github.com>
2022-04-05 23:49:11 +0800

    Time: 231 ms (37.29%), Space: 21.3 MB (26.16%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -1,6 +1,8 @@
 class Solution(object):
     def reverseString(self, s):
-        #type s: List[str]
-        s = s.reverse()
+        #s = s.reverse()
+        
+        for i in range(len(s)/2):
+            s[i], s[-i-1] = s[-i-1], s[i]
         
         
