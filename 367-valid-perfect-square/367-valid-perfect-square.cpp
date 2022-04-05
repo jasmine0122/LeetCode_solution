df983be31e44306cb61706609371796486ee6930
Meow Lu <46015470+jasmine0122@users.noreply.github.com>
2022-04-06 03:57:17 +0800

    Time: 4 ms (25.42%), Space: 5.9 MB (17.16%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -5,9 +5,9 @@ public:
         
         while(low <= high){
             int mid = high + (low - high)/2;
-            if(pow(mid,2) == num)
+            if(pow(mid, 2) == num)
                 return true;
-            else if(pow(mid,2) < num)
+            else if(pow(mid, 2) < num)
                 low = mid + 1;
             else
                 high = mid - 1;
