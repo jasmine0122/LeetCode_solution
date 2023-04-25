43e7d3b4fe75204dae6d8ce8c0c3094d2450bd0e
Meow Lu <46015470+jasmine0122@users.noreply.github.com>
2023-04-26 02:01:37 +0800

    Time: 0 ms (100.00%), Space: 6.3 MB (91.46%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> ans(rowIndex + 1, 1);
+        
+        for(int i=1; i<rowIndex; i++){
+            ans[i] = long(ans[i-1])*(rowIndex-i+1)/i;
+        }
+        return ans;
+    }
+};
