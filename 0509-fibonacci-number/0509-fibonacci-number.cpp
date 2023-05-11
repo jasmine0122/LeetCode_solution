64ea401b00253e520d86acf4ec046766e86aafcc
Meow Lu <46015470+jasmine0122@users.noreply.github.com>
2023-05-11 22:26:05 +0800

    Time: 0 ms (100.00%), Space: 6 MB (67.69%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+        int f[31];
+        f[0] = 0;
+        f[1] = 1;
+        
+        if(n<2)
+            return f[n];
+        
+        for(int i=2; i<=n ;i++)
+            f[i] = f[i-1] + f[i-2];
+        return f[n];
+        
+    }
+};
