e81fac544852e90262ba9373505bf53e3a460056
Meow Lu <46015470+jasmine0122@users.noreply.github.com>
2023-05-11 20:44:16 +0800

    Time: 4 ms (45.47%), Space: 6.3 MB (91.30%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -4,17 +4,10 @@ public:
         int p1=0, p2=0;
         
         while(p1 <= p2 && p1 < s.size() && p2 < t.size()){
-            if(s[p1] == t[p2]){
+            if(s[p1] == t[p2])
                 p1++;
-                p2++;
-            }
-            else{
-                p2++;
-            }
+            p2++;
         }
-        if(p1 == s.size())
-            return true;
-        return false;
-        
+        return p1 == s.size();        
     }
 };
