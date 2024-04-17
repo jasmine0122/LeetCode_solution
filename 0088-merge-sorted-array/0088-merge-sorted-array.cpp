fc8193ff4a7611e9ede2e7f2639d8a2a8dd2c9fc
Meow Lu <46015470+jasmine0122@users.noreply.github.com>
2024-04-18 04:11:31 +0900

    Time: 0 ms (100.00%), Space: 11 MB (31.66%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,9 @@
+// Refer to https://leetcode.com/problems/merge-sorted-array/discuss/1661668/
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        nums1.resize(m);
+        nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+        sort(nums1.begin(), nums1.end());
+    }
+};
