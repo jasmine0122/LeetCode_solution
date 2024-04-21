ab705aa3a6eaf29001902a13142de22db7bf2a68
Meow Lu <46015470+jasmine0122@users.noreply.github.com>
2024-04-21 18:32:49 +0800

    Time: 98 ms (44.40%), Space: 13 MB (7.76%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
@@ -1,16 +1,13 @@
 int maxProfit(int* prices, int pricesSize) {
     int min = pow(10, 4), max = 0;
-    int temp = 0;
     
     for(int i=0; i<pricesSize; i++){
-        if(prices[i]<min){
-            min = prices[i];
-            max = 0;
+        if(prices[i]-min > max){
+            max = prices[i]-min;       
+            continue;
         }
-        if(prices[i]>max)
-            max = prices[i];
-        if(max-min > temp)
-            temp = max-min;
+        if(prices[i]<min)
+            min = prices[i];
     }
-    return temp;
+    return max;
 }
