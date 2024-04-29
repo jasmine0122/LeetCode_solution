36fda2721edd1fe111300a71512cf13043a85885
Meow Lu <46015470+jasmine0122@users.noreply.github.com>
2024-04-29 17:03:33 +0800

    Time: 0 ms (100.00%), Space: 7.9 MB (98.11%) - LeetHub

---
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -0,0 +1,26 @@
+// bit operation
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        int lower = 0, upper = 0;
+        
+        for(int i=0; i<word.size(); i++){
+            if(islower(word[i]))
+                lower |= (1 << word[i]-97);
+            else
+                upper |= (1 << word[i]-65);
+        }
+        // cout<<lower<<endl<<upper<<endl;
+    
+        int count = 0;
+        for(int i=0 ;i<26; i++){
+            if(lower & (1 << i) && upper & (1 << i)){
+                int temp = upper & (1 << i);
+                int temp1 = lower & (1 << i);
+                cout<<temp && temp1;
+                count++;                
+            }
+        }
+        return count;
+    }
+};
