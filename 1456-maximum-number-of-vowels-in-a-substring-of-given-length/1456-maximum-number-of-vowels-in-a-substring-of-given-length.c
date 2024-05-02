c2339d01efb8ad04473058a3035a1ba7abf14e03
Meow Lu <46015470+jasmine0122@users.noreply.github.com>
2024-05-03 00:21:44 +0800

    Time: 427 ms (28.97%), Space: 6.7 MB (89.72%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.c
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.c
@@ -1,7 +1,6 @@
+// Refer to leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/discuss/4715015
 bool judge(char a){
-    if(a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u')    
-        return 1;
-    return 0;
+    return (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u');
 }
 
 int maxVowels(char* s, int k) {
