6b4129114f503e16f262b8ebbe0a0b934a3c61e3
Meow Lu <46015470+jasmine0122@users.noreply.github.com>
2024-05-07 02:35:33 +0800

    Time: 0 ms (100.00%), Space: 5.6 MB (84.36%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.c
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -1,16 +1,14 @@
 char* longestCommonPrefix(char** strs, int strsSize) {
-	for(int c=0;; ++c)
-	{
-		if(strs[0][c] == '\0') 
-			return strs[0];
+    for(int i=0; i<=strlen(strs[0]); i++){
+        if(strs[0][i] == '\0')
+            return strs[0];
         
-		for(int s=1; s<strsSize; ++s)
-		{
-			if(strs[s][c] != strs[0][c]) 
-			{
-				strs[0][c] = '\0';
-				return strs[0];
-			}
-		}
-	}
+        for(int j=1; j<strsSize; j++){
+            if(strs[0][i] != strs[j][i]){
+                strs[0][i] = '\0';
+                return strs[0];
+            }
+        }
+    }
+    return 0;
 }
