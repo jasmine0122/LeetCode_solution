d4efcf082a51652e04b3b506ced2f24965cf39c6
Meow Lu <46015470+jasmine0122@users.noreply.github.com>
2024-05-08 20:20:53 +0800

    Time: 45 ms (74.58%), Space: 8.4 MB (82.50%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.c
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.c
@@ -5,8 +5,11 @@ int longestOnes(int* nums, int numsSize, int k) {
         if(nums[right] == 0){
             k--;
         }
-        if(k < 0 && nums[left++] == 0)
-            k++;        
+        if(k < 0){
+            if(nums[left] == 0)
+                k++;
+            left++;
+        }
         right++;
     }
     return right - left;
