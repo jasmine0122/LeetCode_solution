05be316eac9949d77c82d4d07b66a4dd76228cfb
Meow Lu <46015470+jasmine0122@users.noreply.github.com>
2024-05-12 00:32:23 +0800

    Time: 355 ms (29.48%), Space: 77.7 MB (67.74%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.c
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.c
@@ -5,18 +5,34 @@
  *     struct ListNode *next;
  * };
  */
-struct ListNode* deleteMiddle(struct ListNode* head) {    
+struct ListNode* deleteMiddle(struct ListNode* head) {
+    struct ListNode* CUR = head;
+    int c = 0;
+    
     if(head->next == NULL)
         return NULL;
     
-    struct ListNode* slow = head;
-    struct ListNode* fast = head->next->next;
-    
-    while(fast != NULL && fast->next != NULL){
-        slow = slow->next;
-        fast = fast->next->next;
+    while(CUR != NULL){
+        CUR = CUR->next;
+        c++;
     }
-    slow->next = slow->next->next;
     
+    if(c%2 == 0)
+        c = c/2;
+    else
+        c = (c-1)/2;
+    
+    CUR = head;
+    int i = 0;
+    while(CUR != NULL && CUR->next != NULL){
+        // printf("%d\n", i);
+        if(i == c-1){
+            CUR->next = CUR->next->next;
+            break;
+        }
+        CUR = CUR->next;
+        // printf("hiiiiiiiiiiii\n");
+        i++;
+    }
     return head;
 }
