d872ddf9f6801c1d442ff49061f6ae0f4cd40dc5
jasmine0122 <410621223@gms.ndhu.edu.tw>
2024-05-14 22:55:03 +0800

    [delete] old file

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int titleToNumber(string columnTitle) {
-        int ans = 0;
-        for(char i:columnTitle)
-            ans = ans*26 + (i-'A'+1);
-        return ans;
-    }
-};
--- 344-reverse-string/344-reverse-string.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def reverseString(self, s):
-        #s = s.reverse()
-        
-        for i in range(len(s)/2):
-            s[i], s[-i-1] = s[-i-1], s[i]
-        
-        
