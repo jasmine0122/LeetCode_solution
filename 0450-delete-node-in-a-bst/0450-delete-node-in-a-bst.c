c2e643687f599c09ec40267c16af80798221c9dc
Meow Lu <46015470+jasmine0122@users.noreply.github.com>
2024-05-22 23:56:10 +0800

    Time: 22 ms (21.00%), Space: 13.4 MB (91.91%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.c
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.c
@@ -1,10 +1,6 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
+處理左右子節點都存在的情況，需要在右子樹找到最小值，也就是右子樹中最左下方的節點，
+然後將該最小值賦值給root，然後在右子樹中呼叫遞歸函數來刪除這個值最小的節點
  */
 struct TreeNode* deleteNode(struct TreeNode* root, int key) {
     if(root == NULL)
