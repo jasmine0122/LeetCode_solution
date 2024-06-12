6781d2ac01fb037bb69c2491600e40172b53a7d7
Meow Lu <46015470+jasmine0122@users.noreply.github.com>
2024-06-12 17:17:35 +0800

    Time: 69 ms (44.10%), Space: 21.1 MB (34.03%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.c
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+void split(struct ListNode* top, struct ListNode** a, struct ListNode** b){
+    struct ListNode* fast = top->next;
+    struct ListNode* slow = top;
+    
+    while(fast->next != NULL && fast->next->next != NULL){
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    *a = top;
+    *b = slow->next;
+    slow->next = NULL; //不加的話會stack overflow
+}
+
+struct ListNode* sortedMerge(struct ListNode* a, struct ListNode* b){
+    struct ListNode* result = NULL;
+    
+    if(a == NULL)
+        return b;
+    else if(b == NULL){
+        return a;
+    }
+    
+    if(a->val <= b->val){
+        result = a;
+        result->next = sortedMerge(a->next, b);
+    }
+    else{
+        result = b;
+        result->next = sortedMerge(b->next, a);        
+    }
+    return result;
+}
+
+void mergeSort(struct ListNode** top){
+    struct ListNode* left = NULL;
+    struct ListNode* right = NULL;
+    struct ListNode* curr = *top;
+    
+    if(curr == NULL || curr->next == NULL)
+        return;
+    
+    split(curr, &left, &right);
+    
+    mergeSort(&left);
+    mergeSort(&right);
+    
+    *top = sortedMerge(left, right);
+}
+
+struct ListNode* sortList(struct ListNode* head) {
+    mergeSort(&head);
+    return head;
+}
