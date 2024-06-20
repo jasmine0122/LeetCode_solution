636f40f83bdcbf80cc2602749f983907e5c75fdf
Meow Lu <46015470+jasmine0122@users.noreply.github.com>
2024-06-20 15:54:55 +0800

    Time: 98 ms (49.00%), Space: 21.7 MB (99.10%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.c
+++ 0189-rotate-array/0189-rotate-array.c
@@ -1,8 +1,6 @@
-void reverse(int *nums, int i, int j){
-    int tmp;
-    
-    for( ;i<j ; i++, j--){
-        tmp = nums[i];
+void reverse(int* nums, int i, int j){
+    for(; i<j; i++, j--){
+        int tmp = nums[i];
         nums[i] = nums[j];
         nums[j] = tmp;
     }
@@ -10,6 +8,6 @@ void reverse(int *nums, int i, int j){
 
 void rotate(int* nums, int numsSize, int k) {
     reverse(nums, 0, numsSize-1);
-    reverse(nums, 0, k%numsSize-1);    
-    reverse(nums, k%numsSize, numsSize-1); 
+    reverse(nums, 0, k%numsSize-1);
+    reverse(nums, k%numsSize, numsSize-1);
 }
